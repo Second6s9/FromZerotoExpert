fe7fae1dbf481444b8f79dc13fe3ca6bd80d1676
Second6s9 <Second6s9@163.com>
2022-09-12 20:36:32 +0800

    first web

---
+++ FromZerotoExpert/src/main/java/com/seconds/SecondsApplication.java
@@ -0,0 +1,13 @@
+package com.seconds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondsApplication.class, args);
+    }
+
+}
