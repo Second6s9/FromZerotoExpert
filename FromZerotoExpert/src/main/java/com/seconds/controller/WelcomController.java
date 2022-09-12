8f29697b41069cf4f29ecbfd8aeba20cb9284234
Second6s9 <Second6s9@163.com>
2022-09-12 21:27:30 +0800

    second commit web memory

--- FromZerotoExpert/src/main/java/com/seconds/controller/WelcomController.java
+++ FromZerotoExpert/src/main/java/com/seconds/controller/WelcomController.java
@@ -3,10 +3,29 @@ package com.seconds.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class WelcomController {
     @RequestMapping("/FromZerotoExpert")
-    public String welcom(){
+    public String welcom(HttpServletRequest request, HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        if(null != cookies){
+            for(Cookie cookie : cookies){
+                if("firstLogin".equals(cookie.getName())){
+                    cookie.setValue("1");
+                    cookie.setMaxAge(24 * 60 * 60);
+                    response.addCookie(cookie);
+                    return "index.html";
+                }
+            }
+        }
+
+        Cookie cookie = new Cookie("firstLogin", "0");
+        cookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(cookie);
         return "index.html";
     }
 }
