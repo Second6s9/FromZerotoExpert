b32c1fa7ce9117374a384ff55f6b6e3c7af4a3e8
Second6s9 <Second6s9@163.com>
2022-09-15 14:49:15 +0800

    day05 register function second commit

--- FromZerotoExpert/from_zeroto_expert.sql
+++ FromZerotoExpert/from_zeroto_expert.sql
@@ -4,6 +4,7 @@ CREATE TABLE user(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	username varchar(255) DEFAULT(NULL),
 	password varchar(255) DEFAULT(NULL),
+	email varchar(255) DEFAULT(NULL),
 	salt varchar(255) DEFAULT(NULL),
 	activeStatus INT DEFAULT(0)
 )ENGINE=INNODB;
