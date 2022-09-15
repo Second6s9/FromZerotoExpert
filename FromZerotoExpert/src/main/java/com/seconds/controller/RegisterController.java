4fd90504455855e77d4ffb3998ca4265c59ae8fb
Second6s9 <Second6s9@163.com>
2022-09-15 15:20:28 +0800

    day05 register function fourth commit

--- FromZerotoExpert/src/main/java/com/seconds/controller/RegisterController.java
+++ FromZerotoExpert/src/main/java/com/seconds/controller/RegisterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/FromZerotoExpert")
-public class UserController {
+public class RegisterController {
     @Autowired
     private Register register;
 
